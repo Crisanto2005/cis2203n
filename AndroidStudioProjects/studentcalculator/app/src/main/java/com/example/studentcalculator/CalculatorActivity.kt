d3a0aea372aa9485d8d0951128c27548adbebe30
elcrisrazonable7@gmail.com <elcrisrazonable7@gmail.com>
2026-04-21 19:10:06 +0800

    kotlin-01-task-a1-calc-var

---
+++ AndroidStudioProjects/studentcalculator/app/src/main/java/com/example/studentcalculator/CalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.studentcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class CalculatorActivity : AppCompatActivity() {
+
+    var currentInput = ""
+    var firstOperand = 0.0
+    var currentOperator = ""
+
+}
